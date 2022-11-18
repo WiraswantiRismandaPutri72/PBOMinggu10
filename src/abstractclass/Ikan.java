40acdf814141a93161912e658e997b6a66e73240
Wiraswanti Rismanda Putri <wiraswantirismandaputri@gmail.com>
2022-11-18 09:45:09 +0700

    first commit

---
+++ src/abstractclass/Ikan.java
@@ -0,0 +1,9 @@
+package abstractclass;
+
+public class Ikan extends Hewan{
+    
+    @Override
+    public void bergerak(){
+        System.out.println("Berenang dengan SIRIP, \"Wush..Wush..\"");
+    }
+}
